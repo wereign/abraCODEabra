0e4d7d1a4e2c218ecdc89b5b9229db9629d5f3c4
Virenn Jay <101888489+wereign@users.noreply.github.com>
2022-04-23 19:37:16 +0530

    Add files via upload
    Makes a .csv database file.

---
+++ database_maker.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import random
+
+names = list(pd.read_csv("./baby-names.csv")["name"][:1000])
+cities = list(pd.read_csv("./worldcities.csv")["city"][:1000])
+
+INCOME_UPPER = 1_000_000
+INCOME_LOWER = 1000
+incomes = []
+for i in range(1000):
+    incomes.append(random.randint(INCOME_LOWER,INCOME_UPPER))
+
+
+# Age
+AGE_LOWER = 15
+AGE_UPPER = 101
+ages = []
+for i in range(1000):
+    ages.append(random.randint(AGE_LOWER,AGE_UPPER))
+
+# Receiving the pension already?
+# TODO: Eliminate possibilty of true for a person below 60 or earning a lot
+receiving = [True, False]
+recips = []
+for i in range(1000):
+    recips.append(random.choice(receiving))
+
+data = {
+    'name': names,
+    'age': ages,
+    "income": incomes,
+    "receiving pension": recips,
+    "cities": cities,
+    }
+
+final_dataset = pd.DataFrame(data)
+final_dataset.to_csv("./citizens_database.csv")
+print(final_dataset)
