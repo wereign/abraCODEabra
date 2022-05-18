e48507dd063abfa25c48bfedc0f003ce6eb5c780
wereign <virenn9999@gmail.com>
2022-05-18 14:40:51 +0530

    Names

--- database_maker.py
+++ database_maker.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import random
 
 
@@ -23,12 +24,37 @@ class Dataset:
         self.names_list = list(pd.read_csv(names_file)["name"])
         self.areas_list = list(pd.read_csv(area_file)["city"][800:1000])
 
+        self.NSAP_Names = []
+        self.NSAP_Area = []
+        self.NSAP_Incomes = []
+        self.NSAP_Ages = []
+
+    def make_nsap_database(self):
+
+        for step in range(2000):
+            self.NSAP_Names.append(random.choice(self.names_list))
+            self.NSAP_Area.append(random.choice(self.areas_list))
+            self.NSAP_Incomes.append(random.randint(2000, 11000))
+            self.NSAP_Ages.append(random.randint(60, 101))
+
+        nsap = {
+            ""
+            'name': self.NSAP_Names,
+            'age': self.NSAP_Ages,
+            "income": self.NSAP_Incomes,
+            "area": self.NSAP_Area,
+        }
+
+        nsap_dataframe = pd.DataFrame(nsap)
+        return nsap_dataframe
+
+
     def make_database(self):
         for step in range(self.size):
             self.names.append(random.choice(self.names_list))
             self.areas.append(random.choice(self.areas_list))
             self.incomes.append(random.randint(self.income_lower, self.income_upper))
-            self.recips.append(random.choice(self.receiving))
+            # self.recips.append(random.choice(self.receiving))
             self.ages.append(random.randint(self.age_lower, self.age_upper))
 
         data = {
@@ -36,14 +62,21 @@ class Dataset:
             'name': self.names,
             'age': self.ages,
             "income": self.incomes,
-            "receiving_pension": self.recips,
+            # "receiving_pension": self.recips,
             "area": self.areas,
         }
+        temp_data = pd.DataFrame(data)
+        temp_nsap = self.make_nsap_database()
+        final_dataset = temp_data.append(temp_nsap)
+        # final_dataset.to_csv("./citizens_database.csv"
 
-        final_dataset = pd.DataFrame(data)
-        # final_dataset.to_csv("./citizens_database.csv")
-        return final_dataset
+        # dataset["receiving_pension"] = np.where(dataset.age >= 60,)
+        return temp_nsap,final_dataset
 
-# dataset = Dataset(size_dataset=100,income_lower=100,income_upper=1000,age_lower=15,age_higher=69)
-# new = dataset.make_database()
-# print(new)
+
+# dataset = Dataset(size_dataset=100, income_lower=100, income_upper=1000, age_lower=15, age_higher=69)
+# data = dataset.make_database()
+# print(data)
+
+# inserting all the nsap database names into the main database, adding more eligible persons.
+# use the nsap databse to assign a true value for the values in the main database.
--- main.py
+++ main.py
@@ -1,17 +1,21 @@
+import pandas as pd
+
 from database_maker import Dataset
 
 AGE_OLD = 60   # Current cut off age for NSAP
 INCOME_BPL = 11000   # Made up BPL criteria
 
 
-dataset = Dataset(size_dataset=10000,
+datasets = Dataset(size_dataset=1000,
                   income_lower=1000, income_upper=20000,
                   age_lower=15, age_higher=100).make_database()
 
+nsap_dataset = datasets[0].to_dict()
+dataset = datasets[1]
+
 # Current age filter > 60, income less than 11,000.
 filtered_dataset = dataset[(dataset.age >= AGE_OLD) &
-                           (dataset.income <= INCOME_BPL) &
-                           (dataset.receiving_pension == "Not Receiving")]
+                           (dataset.income <= INCOME_BPL)]
 
 # Grouping the data area wise
 grouped_dataset = filtered_dataset.groupby("area")
@@ -24,9 +28,39 @@ for area_ in all_areas:
     temp = grouped_dataset.get_group(area_)
     area_wise.append(temp)
 
-for area in area_wise:
-    print("____________NEW CITY_____________")
-    print()
-    print()
-    print(area)
+# for area in area_wise:
+#     print("____________NEW CITY_____________")
+#     print()
+#     print()
+#     print(area)
+
+one_place = area_wise[0]
+place_dict = one_place.to_dict()
+# print(place_dict)
+
+final_names = []
+final_ages = []
+final_income = []
+final_area = []
+
+for key in place_dict['name']:
+    if not ((place_dict['name'][key] in nsap_dataset['name']) and (place_dict['age'][key] in nsap_dataset['age']) and
+            (place_dict['income'][key] in nsap_dataset['income']),
+            (place_dict['area'][key] in nsap_dataset['area'])):
+
+        final_names.append(place_dict['name'][key])
+        final_ages.append(place_dict['age'][key])
+        final_income.append(place_dict['income'][key])
+
+        area_temp = pd.DataFrame(data={"NAME":final_names, "AGE":final_ages})
+        print("Here")
+        print(area_temp)
+
+    else:
+        pass
+
+
+
+# making an NSAP dataset.
+
 
